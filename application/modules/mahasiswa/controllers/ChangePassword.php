65869d127f58b608fab8cccd97d340563beb4d8a
Nadya Prabaningrum <nadya.prabaningrum@mail.ugm.ac.id>
2018-03-23 19:05:56 +0700

    change password done :astonished:

---
+++ application/modules/mahasiswa/controllers/ChangePassword.php
@@ -0,0 +1,96 @@
+<?php if(!defined('BASEPATH')) exit('No direct script access allowed');
+require APPPATH . '/libraries/BaseController.php';
+/**
+ * Created by nad.
+ * Date: 23/03/2018
+ * Time: 18:22
+ * Description:
+ */
+
+class ChangePassword extends BaseController
+{
+    /**
+     * This is default constructor of the class
+     */
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('change_password_model');
+        $this->isLoggedIn();
+    }
+
+    /**
+     * This function is used to load the change password screen
+     */
+    function loadChangePass()
+    {
+        if($this->isMahasiswa() == TRUE)
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->global['pageTitle'] = 'Komsi : Change Password';
+
+            $this->loadViews("change_password", $this->global, NULL, NULL);
+        }
+    }
+
+
+    /**
+     * This function is used to change the password of the user
+     */
+    function changePassword()
+    {
+        if($this->isMahasiswa() == TRUE)
+        {
+            $this->loadThis();
+        }
+        else
+        {
+            $this->load->library('form_validation');
+
+            $this->form_validation->set_rules('oldPassword','Old password','required|max_length[20]');
+            $this->form_validation->set_rules('newPassword','New password','required|max_length[20]');
+            $this->form_validation->set_rules('cNewPassword','Confirm new password','required|matches[newPassword]|max_length[20]');
+
+            if($this->form_validation->run() == FALSE)
+            {
+                $this->loadChangePass();
+            }
+            else
+            {
+                $oldPassword = $this->input->post('oldPassword');
+                $newPassword = $this->input->post('newPassword');
+
+                $resultPas = $this->change_password_model->matchOldPassword($this->vendorId, $oldPassword);
+
+                if(empty($resultPas))
+                {
+                    $this->session->set_flashdata('nomatch', 'Your old password not correct');
+                    redirect('mahasiswa/changepassword/loadChangePass');
+                }
+                else
+                {
+                    $usersData = array('password'=>getHashedPassword($newPassword), 'updatedDtm'=>date('Y-m-d H:i:s'));
+
+                    $result = $this->change_password_model->changePassword($this->vendorId, $usersData);
+
+                    if($result > 0) { $this->session->set_flashdata('success', 'Password updation successful'); }
+                    else { $this->session->set_flashdata('error', 'Password updation failed'); }
+
+                    redirect('mahasiswa/changepassword/loadChangePass');
+                }
+            }
+        }
+    }
+
+    function pageNotFound()
+    {
+        $this->global['pageTitle'] = 'Komsi : 404 - Page Not Found';
+
+        $this->loadViews("404", $this->global, NULL, NULL);
+    }
+}
+
+?>
